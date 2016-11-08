823ca29b0aa1a8053a236e30f49a5eba7282e9b4
Peter Tountas <petertountas1@gmail.com>
2016-11-07 21:52:55 -0600

    more

--- assets/logic.js
+++ assets/logic.js
@@ -3,12 +3,16 @@ $(document).ready(function(){
 
 
 	$('button').on('click',function(){
+		//test to make sure the on click functionality is working
+		console.log('test');
+
 		
 		// ARRAY TO PUSH INPUT INTO ARRAY AND HOLD PREVIOUS SEARCHES
 		var prevGifsSearched = [];
 
 		//APPLY SEARCH INPUT TO VARIABLE
-		var getGif = $(this).data('gifs');
+		var getGif = document.getElementById('inputBox').value;
+
 
 		//API URL QUERY
 		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + getGif + "&api_key=dc6zaTOxFJmzC&limit=10"
