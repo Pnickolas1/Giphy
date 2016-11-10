efa31c624520be7a3d51d4217d6dc4b7e4e32be5
Peter Tountas <petertountas1@gmail.com>
2016-11-09 20:16:06 -0600

    more changes

--- assets/logic.js
+++ assets/logic.js
@@ -1,30 +1,73 @@
 $(document).ready(function(){
 	console.log("document loaded");
 
+// SET AN ARRAY WITH A NUMBER OF ANIMALS
+var gifList = ['Tigers','Lion','Sloth','Donald Trump']
 
-	$('button').on('click',function(){
-		//test to make sure the on click functionality is working
-		console.log('test');
 
+function displayGifs(){
+	var gif = $(this).attr('data-gifs');
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=dc6zaTOxFJmzC&limit=10"
+
+	//CALL GIPHY AJAX
+	$.ajax({url: queryURL, metho: 'GET'}).done(function(response){
+			console.log(response);
+
+	});
+}
+
+
+
+
+function renderButtons(){
+
+	// DELETES THE GIFS PRIOR TO ADDING NEW MOVIES 
+	$("#gifsButtons").empty();
+
+	//LOOPS THROUGH THE ARRAY OF GIFS SEARCH
+	for(var i = 0; i < gifList.length; i++){
+
+		var a = $('<button>')
+
+		// Class="giphys"
+		a.addClass('giphys') ;
+
+		// add data-gifs attribute
+		a.attr('data-gifs');
 		
-		// ARRAY TO PUSH INPUT INTO ARRAY AND HOLD PREVIOUS SEARCHES
-		var prevGifsSearched = [];
+		//index thru array and nunber of items
+		a.text(gifList[i]);
+	
+		$('#gifsButtons').append(a);
 
-		//APPLY SEARCH INPUT TO VARIABLE
-		var getGif = document.getElementById('inputBox').value;
+	}
+
+} //ENDS renderButtons FUNCTION
 
 
-		//API URL QUERY
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + getGif + "&api_key=dc6zaTOxFJmzC&limit=10"
+
+$('#addGif').on('click',function(){
+
+	// SETS VARIABLE FROM INPUT BOX
+	var gifSearch = $('#inputBox').val().trim();
+
+	// PUSH gifSearch INTO gifList ARRAY
+	gifList.push(gifSearch);
+	console.log(gifList);
+
+	//RENDER BUTTONS
+	renderButtons();
+
+	// USER CAN HIT ENTER TO SUBMIT SEARCH FOR GIFS AND ADD BUTTON
+	return false;
+})
+
+$(document).on('click','.giphys',displayGifs);
+
+renderButtons();
 
 
-			$.ajax({
-				url: queryURL,
-				method: 'GET',
-			})
-				.done(function(response){
-				 var results = response.data;	
 
-				})
-	});  // Ends $('button') LINE 5
 }); // Ends Script from Document.ready - LINE 1
+
+// http://api.giphy.com/v1/gifs/search?q=kanye&api_key=dc6zaTOxFJmzC&limit=10
